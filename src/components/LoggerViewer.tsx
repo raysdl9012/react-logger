5ca1845565ca95370928004b47c0b688767eb25d
Reinner Steven Daza Leiva <reinner.leiva@outlook.com>
2026-02-13 10:38:57 -0500

    fix: Android rendering artifacts and mobile optimization (v1.1.2)

--- src/components/LoggerViewer.tsx
+++ src/components/LoggerViewer.tsx
@@ -13,7 +13,7 @@ export const LoggerViewer: React.FC = () => {
 
     return (
         <>
-            <FloatingButton />
+            {!isPanelOpen && <FloatingButton />}
             {isPanelOpen && <LogPanel />}
         </>
     );
